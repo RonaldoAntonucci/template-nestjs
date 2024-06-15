78b56312aa6cf9bc2b81a5d3efc1d48efd83ad29
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2024-06-15 17:35:48 -0300

    chore: fix e2e problems

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-    imports: [SharedModule, AuthModule],
+    imports: [SharedModule, AuthModule, HealthModule],
 })
 export class AppModule {}
