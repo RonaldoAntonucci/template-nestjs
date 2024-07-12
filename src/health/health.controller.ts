f2e10be9c134732848abcddd2b98decb397dd022
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2024-07-12 12:29:01 -0300

    feat: add TypeOrmHealthIndicator to health controller

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,17 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
+import {
+    HealthCheckService,
+    HealthCheck,
+    TypeOrmHealthIndicator,
+    HealthIndicatorResult,
+} from '@nestjs/terminus';
 import { Public } from 'src/auth/auth.decorator';
 
 @Controller('health')
 @ApiTags('health')
 export class HealthController {
-    constructor(private health: HealthCheckService) {}
+    constructor(
+        private health: HealthCheckService,
+        private db: TypeOrmHealthIndicator,
+    ) {}
 
     @Get()
     @Public()
     @HealthCheck()
     check(): ReturnType<HealthCheckService['check']> {
-        return this.health.check([]);
+        return this.health.check([
+            (): Promise<HealthIndicatorResult> => this.db.pingCheck('database'),
+        ]);
     }
 }
